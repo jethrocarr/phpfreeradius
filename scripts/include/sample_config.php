dc528f1279a6cd71e10e7fd64a1a38bc9c065e7f
Jethro Carr <jethro.carr@jethrocarr.com>
2010-04-19 23:27:15 +0000

    * Added new configuration options to sample config file

--- scripts/include/sample_config.php
+++ scripts/include/sample_config.php
@@ -12,9 +12,37 @@
 /*
 	API Configuration
 */
-$config["api_url"]		= "http://devel-webapps.local.amberdms.com/development/amberdms/phpfreeradius/htdocs/";		// Application Install Location
-$config["api_server_name"]	= "devel-auth-openldap.local.amberdms.com";
-$config["api_auth_key"]		= "ultrahighsecretkeyok";
+$config["api_url"]		= "http://example.com/phpfreeradius";			// Application Install Location
+$config["api_server_name"]	= "radius.example.com";
+$config["api_auth_key"]		= "ultrahighsecretkey";
+
+
+/*
+	LDAP Configuration
+*/
+$config["ldap_groupdn"]		= "ou=Group,ou=auth,dc=example,dc=com";
+
+
+/*
+	Log Pipe File
+*/
+$config["log_pipe"]		= "/var/run/phpfreeradius_log";
+$config["log_owner"]		= "radiusd";
+
+
+
+/*
+	Radius Configuration Files
+*/
+
+$config["freeradius_format"]		= "1";				// version/format of freeradius config files - either 1 (1.x.x) or 2 (2.x.x)
+$config["freeradius_file_clients"]	= "/etc/raddb/clients.conf";	// clients/NAS configuration file
+$config["freeradius_file_huntgroups"]	= "/etc/raddb/huntgroups";	// huntgroups
+$config["freeradius_file_users"]	= "/etc/raddb/users";		// users
+$config["freeradius_reload"]		= "/etc/init.d/radiusd reload";	// command to reload radius configuration
+
+
+
 
 // force debugging on for all users + scripts
 // (note: debugging can be enabled on a per-user basis by an admin via the web interface)
